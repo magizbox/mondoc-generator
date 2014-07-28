c7370765935bc4362a9babb80756c009df23b182
Brother Rain <brother.rain.1024@gmail.com>
2014-07-28 15:41:08 +0700

    Update main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,6 @@
 
 angular.module('docsGeneratorApp')
   .controller('MainCtrl', function ($scope) {
-<<<<<<< HEAD
 
      $scope.currentFunction = 
    {"name":"each","code":"_.each([1, 2, 3], alert);\n=> alerts each number in turn...\n_.each({one: 1, two: 2, three: 3}, alert);\n=> alerts each number value in turn...","signature":"_.each(list, iterator, [context])","description":"Iterates over a list of elements, yielding each in turn to an iterator function. The iterator is bound to the context object, if one is passed. Each invocation of iterator is called with three arguments: (element, index, list). If list is a JavaScript object, iterator's arguments will be (value, key, list). Delegates to the native forEach function if it exists, and returns the original list for chaining."};
@@ -19,8 +18,6 @@ angular.module('docsGeneratorApp')
             console.log(e);
          };
       }
-=======
      $scope.currentFunction = 
    {"name":"each","code":"_.each([1, 2, 3], alert);\n=> alerts each number in turn...\n_.each({one: 1, two: 2, three: 3}, alert);\n=> alerts each number value in turn...","signature":"_.each(list, iterator, [context])","description":"Iterates over a list of elements, yielding each in turn to an iterator function. The iterator is bound to the context object, if one is passed. Each invocation of iterator is called with three arguments: (element, index, list). If list is a JavaScript object, iterator's arguments will be (value, key, list). Delegates to the native forEach function if it exists, and returns the original list for chaining."}
->>>>>>> 4ac1921fb235734ce7a888231a3c12ebe12b4c07
   });
